53a57fd53a3a40e3065335455ae2fda379dcc312
Diana Liao <dijali@gmail.com>
2021-02-14 23:25:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,6 +2,15 @@
 
 def run_guessing_game
   
+  to_guess = rand(1..6)
   
+  puts "Guess the number from 1 to 6"
+  guess = gets.chomp
   
+  if guess = "exit"
+    puts "Goodbye!"
+  elsif guess == to_guess
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{to_guess}."
 end
